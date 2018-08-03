46a4393a482bae326ef8e3460965f2227879ac57
Jsownz <jason@frozensoliddesigns.com>
2018-08-02 20:24:44 -0700

    constant listening until 'stop listening' is requested

--- main.py
+++ main.py
@@ -4,46 +4,55 @@ import os
 import sys
 import win32com.client
 
-# set config file
-config_file = "./config.json"
-config_data = {}
-# declare recognizer
-r = sr.Recognizer()
-# create speaker
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
+def listen_for_input(r, mic):
+    # listen to input
+    print("Now listening...")
+    with mic as source:
+        audio = r.listen(source)
 
-if (os.path.isfile(config_file)):
-    # load config
-    with open(config_file) as json_data:
-        config_data = json.load(json_data)
-else:
-    # didnt find a config file, get mic selection
-    print("No config file found. Please select a microphone to use")
-    print("-------------------------------------------------------")
-    # loop over mic options and print them out nicely
-    for index,choice in enumerate(sr.Microphone.list_microphone_names()):
-        print("%s - %s" % (index,choice))
+    # print recognized audio
+    recognized_speech = r.recognize_google(audio)
+    print(recognized_speech)
+
+    if (recognized_speech.lower() == "stop listening"):
+        speaker.Speak("Goodbye")
+        sys.exit(0)
     
-    # get user mic choice
-    mic_choice = int(input("Select the index of the mic: "))
+    listen_for_input(r, mic)
 
-    # build the data
-    config_data['microphone_index'] = mic_choice
+if __name__ == "__main__":
+    # set config file
+    config_file = "./config.json"
+    config_data = {}
+    # declare recognizer
+    r = sr.Recognizer()
+    # create speaker
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
-    # the create config file
-    # and write mic choice to it
-    with open(config_file, 'w') as outfile:
-        json.dump(config_data, outfile)
+    if (os.path.isfile(config_file)):
+        # load config
+        with open(config_file) as json_data:
+            config_data = json.load(json_data)
+    else:
+        # didnt find a config file, get mic selection
+        print("No config file found. Please select a microphone to use")
+        print("-------------------------------------------------------")
+        # loop over mic options and print them out nicely
+        for index,choice in enumerate(sr.Microphone.list_microphone_names()):
+            print("%s - %s" % (index,choice))
+        
+        # get user mic choice
+        mic_choice = int(input("Select the index of the mic: "))
 
-# declare mic to use
-mic = sr.Microphone(device_index=int(config_data['microphone_index']))
+        # build the data
+        config_data['microphone_index'] = mic_choice
 
-# listen to input
-print("Now listening...")
-with mic as source:
-   audio = r.listen(source)
+        # the create config file
+        # and write mic choice to it
+        with open(config_file, 'w') as outfile:
+            json.dump(config_data, outfile)
 
-# print recognized audio
-recognized_speech = r.recognize_google(audio)
-print(recognized_speech)
-speaker.Speak("I got your input")
+    # declare mic to use
+    mic = sr.Microphone(device_index=int(config_data['microphone_index']))
+
+    listen_for_input(r, mic)
