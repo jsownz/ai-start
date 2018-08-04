09afeb4d9410a97e809bc0de0017ff73f40e2fd5
Jason Sohl <jason@frozensoliddesigns.com>
2018-08-03 19:00:18 -0700

    adding macos speech support

--- speaker.py
+++ speaker.py
@@ -3,8 +3,13 @@ import sys
 def speak(words):
   if sys.platform in ['Windows', 'win32', 'cygwin']:
       import win32com.client
-      
+
       # create speaker
       speaker = win32com.client.Dispatch("SAPI.SpVoice")
       speaker.Speak(words)
-  
+  elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
+    from os import system
+    system("say %s" % words)
+
+  elif sys.platform in ['linux', 'linux2']:
+    print("Not implemented yet")
